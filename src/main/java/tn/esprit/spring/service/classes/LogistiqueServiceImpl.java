ef72338808f368d8e7ec004202cbafa4cc456515
NourhenHamza <130661537+NourhenHamza@users.noreply.github.com>
2025-10-16 13:31:15 +0100

    Ajout des logs dans LogistiqueServiceImpl

--- src/main/java/tn/esprit/spring/service/classes/LogistiqueServiceImpl.java
+++ src/main/java/tn/esprit/spring/service/classes/LogistiqueServiceImpl.java
@@ -25,33 +25,66 @@ public class LogistiqueServiceImpl implements ILogistiqueService {
 	
 	@Override
 	public Logistique ajoutAffectLogEven(Logistique l, String description_evnm) {
+		log.info("Début de l'ajout et affectation de logistique pour l'événement: {}", description_evnm);
+		
 		Evenement e = evenRep.findByDescription(description_evnm);
+		
+		if (e == null) {
+			log.error("Événement non trouvé avec la description: {}", description_evnm);
+			return null;
+		}
+		
+		log.info("Événement trouvé: {} - ID: {}", e.getDescription(), e.getId());
+		
 		List<Logistique> logis;
 		if (e.getLogistiques() == null) {
-		    logis = new ArrayList<>();
-		} 
-		else {
-			logis= e.getLogistiques();}
-			logistiqueRepository.save(l);
-			logis.add(l);
-			e.setLogistiques(logis);
-			evenRep.save(e);
-			return l;
+			log.info("Aucune logistique existante pour cet événement, création d'une nouvelle liste");
+			logis = new ArrayList<>();
+		} else {
+			log.info("Liste de logistiques existante trouvée avec {} élément(s)", e.getLogistiques().size());
+			logis = e.getLogistiques();
+		}
+		
+		logistiqueRepository.save(l);
+		log.info("Logistique sauvegardée avec succès - ID: {}", l.getId());
+		
+		logis.add(l);
+		e.setLogistiques(logis);
+		evenRep.save(e);
+		
+		log.info("Logistique affectée à l'événement avec succès. Total logistiques: {}", logis.size());
+		return l;
 	}
 
 	@Override
 	public List<Logistique> getLogistiquesDates(Date dated, Date datef) {
-		List <Evenement> events = evenRep.findByDatedBetween(dated, datef);
-		List <Logistique> allLogists=new ArrayList<>();
-		for (Evenement e: events)
-		{
-			for(Logistique l: e.getLogistiques())
-			{if (l.isReserve())
-			allLogists.add(l);
-		 }
+		log.info("Récupération des logistiques réservées entre {} et {}", dated, datef);
+		
+		List<Evenement> events = evenRep.findByDatedBetween(dated, datef);
+		log.info("Nombre d'événements trouvés dans cette période: {}", events.size());
+		
+		List<Logistique> allLogists = new ArrayList<>();
+		int totalLogistiques = 0;
+		int logistiquesReservees = 0;
+		
+		for (Evenement e : events) {
+			log.debug("Traitement de l'événement: {} - Date: {}", e.getDescription(), e.getDated());
+			
+			if (e.getLogistiques() != null) {
+				totalLogistiques += e.getLogistiques().size();
+				
+				for (Logistique l : e.getLogistiques()) {
+					if (l.isReserve()) {
+						allLogists.add(l);
+						logistiquesReservees++;
+						log.debug("Logistique réservée ajoutée - ID: {}", l.getId());
+					}
+				}
+			}
+		}
+		
+		log.info("Résultat: {} logistiques réservées trouvées sur {} logistiques totales", 
+				logistiquesReservees, totalLogistiques);
+		return allLogists;
+	}
 }
-		return allLogists;}}
-	
-	
-
-	
