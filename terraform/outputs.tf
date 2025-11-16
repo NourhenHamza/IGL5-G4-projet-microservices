c07c27bac752888958baf639d2bc37bbb88e2570
abidnouha55@gmail.com <abidnouha55@gmail.com>
2025-11-16 23:53:58 +0100

    màj files terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,9 @@
 output "EC2_public_ip" {
-  value = aws_instance.docker_host.public_ip
+description = "The public IP address of the general-purpose Docker host EC2 instance."
+value       = aws_instance.docker_host.public_ip
 }
 
 output "vpc_id" {
-  value = aws_vpc.main.id
+description = "The ID of the VPC created."
+value       = aws_vpc.main.id
 }
