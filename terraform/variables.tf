9bd61bb7f7fe73f14f0679d598acff4a859d24bd
abidnouha55@gmail.com <abidnouha55@gmail.com>
2025-11-22 00:05:50 +0100

    Update EKS role ARNs in variables.tf for consistency

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -53,11 +53,11 @@ variable "node_max" {
 variable "eks_role_arn" {
   description = "ARN of pre-created IAM role for EKS control plane"
   type        = string
-  default     = "arn:aws:iam::969827070936:role/c180773a4650446l12670702t1w969827-LabEksClusterRole-ih3G2lnJjGiT"
+  default     = "arn:aws:iam::969827070936:role/c180773a4650446l12670702t1w969827-LabEksClusterRole-AI0BbpjQafL5"
 }
 
 variable "eks_node_role_arn" {
   description = "ARN of pre-created IAM role for EKS worker nodes"
   type        = string
-  default     = "arn:aws:iam::969827070936:role/c180773a4650446l12670702t1w969827070-LabEksNodeRole-gfigi17UVOqE"
+  default     = "arn:aws:iam::969827070936:role/c180773a4650446l12670702t1w969827070-LabEksNodeRole-6OOzrLrRdfx3"
 }
