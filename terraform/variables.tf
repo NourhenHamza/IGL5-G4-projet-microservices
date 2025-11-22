6512160aed14c78fd31684fc720b5a53aeb9d514
abidnouha55@gmail.com <abidnouha55@gmail.com>
2025-11-22 09:29:07 +0100

    Refactor Kubernetes deployment and service names for consistency; update EKS role ARNs in variables.tf for accuracy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -53,11 +53,11 @@ variable "node_max" {
 variable "eks_role_arn" {
   description = "ARN of pre-created IAM role for EKS control plane"
   type        = string
-  default     = "arn:aws:iam::969827070936:role/c180773a4650446l12670702t1w969827-LabEksClusterRole-vKcndjcAji4O"
+  default     = "arn:aws:iam::969827070936:role/c180773a4650446l12670702t1w969827-LabEksClusterRole-O1JZcAA2ygEp"
 }
 
 variable "eks_node_role_arn" {
   description = "ARN of pre-created IAM role for EKS worker nodes"
   type        = string
-  default     = "arn:aws:iam::969827070936:role/c180773a4650446l12670702t1w969827070-LabEksNodeRole-JKUidznJijah"
+  default     = "arn:aws:iam::969827070936:role/c180773a4650446l12670702t1w969827070-LabEksNodeRole-M5ZSxSZxQjw4"
 }
